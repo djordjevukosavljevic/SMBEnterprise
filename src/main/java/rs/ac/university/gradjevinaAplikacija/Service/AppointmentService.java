2e7b61168e416e696f33dd501641953d75cfb8c5
Djordje Vukosavljevic <you@example.com>
2025-04-05 23:14:17 +0200

    small updates

--- src/main/java/rs/ac/university/gradjevinaAplikacija/Service/AppointmentService.java
+++ src/main/java/rs/ac/university/gradjevinaAplikacija/Service/AppointmentService.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class AppointmentService
+public class   AppointmentService
 {
     private final AppointmentRepository repository;
 
