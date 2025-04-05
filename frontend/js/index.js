4a860cd2cec809ab74a1bc98cd969691ccc90484
Djordje Vukosavljevic <you@example.com>
2025-04-06 01:48:56 +0200

    small updates

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,7 +2,9 @@ const list = document.getElementById('list')
 fetch('http://localhost:8082/api/person')
     .then(rsp => rsp.json())
     .then(data => {
-        data.foreach(person => {
+        data.forEach(element => {
+
+        });(person => {
             li.innerText = `${person.id} ${person.name} ${person.surrname} ${person.email} ${person.number}`
             list.appendChild(li)
         })
