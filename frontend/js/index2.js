a0265a62dfa19f7ae8d2d0030b40a6be81113103
Djordje Vukosavljevic <you@example.com>
2025-04-08 12:15:37 +0200

    wtf

--- frontend/js/index2.js
+++
@@ -1,8 +0,0 @@
-const applist = document.getElementById("applist")
-fetch('http://localhost:8082/api/appointment')
-    .then(rsp => rsp.json())
-    .then(data => {
-        data.forEach(appointment => {
-            console.log(data)
-        })
-    })
