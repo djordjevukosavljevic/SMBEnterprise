fe123ea90d2d237496e96c7b75899167a320823c
Djordje Vukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-05-13 10:08:08 +0200

    added appointments to navbar

--- src/main/java/rs/ac/university/gradjevinaAplikacija/GradjevinaAplikacijaApplication.java
+++ src/main/java/rs/ac/university/gradjevinaAplikacija/GradjevinaAplikacijaApplication.java
@@ -6,9 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GradjevinaAplikacijaApplication
 {
+
 	public static void main(String[] args)
 	{
 		SpringApplication.run(GradjevinaAplikacijaApplication.class, args);
 	}
-
+	
 }
