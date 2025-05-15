d185f42aeefa305d4c070da1f06cdc38f8d85088
Djordje Vukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-05-16 00:13:36 +0200

    adding default view page

--- src/main/java/rs/ac/university/gradjevinaAplikacija/Controller/HomeController.java
+++ src/main/java/rs/ac/university/gradjevinaAplikacija/Controller/HomeController.java
@@ -1,10 +1,10 @@
 package rs.ac.university.gradjevinaAplikacija.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping(path="/api/index")
 public class HomeController
 {
