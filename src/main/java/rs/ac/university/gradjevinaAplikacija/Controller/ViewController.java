d8f74bd3698486ef97a0d59e02d35c102960c342
Djordje Vukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-05-16 14:12:54 +0200

    Image frontend changes

---
+++ src/main/java/rs/ac/university/gradjevinaAplikacija/Controller/ViewController.java
@@ -0,0 +1,15 @@
+package rs.ac.university.gradjevinaAplikacija.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController
+{
+
+    @GetMapping("/image-view")
+    public String imageView()
+    {
+        return "image";
+    }
+}
