6e18ebe74a4de6049f71fa11fd6dfb0e1da084ed
Djordje Vukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-06-04 19:13:32 +0200

    trying to change the look

--- src/main/java/rs/ac/university/gradjevinaAplikacija/Controller/HomeController.java
+++ src/main/java/rs/ac/university/gradjevinaAplikacija/Controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(path="/api/index")
 public class HomeController
 {
-
+    
     @GetMapping
     public String defaultView(){
         return "index";
