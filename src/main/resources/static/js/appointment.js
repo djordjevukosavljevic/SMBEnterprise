5015962b28ef3507bdf919eb92373c0874d17fb8
zorzezorz <djordje.vukosavljevic01@gmail.com>
2025-06-05 22:08:53 +0200

    fetching appointments

--- src/main/resources/static/js/appointment.js
+++ src/main/resources/static/js/appointment.js
@@ -1,23 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
     fetch("http://localhost:8082/api/appointment")
         .then(response => {
-            if (!response) {
+            if (!response.ok) {
                 throw new Error('Network error was not resolved');
             }
             return response.json();
+
         })
         .then(data => {
             const tableBody = document.getElementById("appointmentTableBody");
-            tableBody.innerHTML = "";   // Clear existing
+            tableBody.innerHTML = '';   // Clear existing
 
             data.forEach(appointment => {
+                console.log(appointment);
                 const row = document.createElement('tr');
 
-                row.innerHTML = 
-                `
-                <th></th>
-                <td>${appointment.createdAt}<td>
-                <td>${appointment.updatedAt}<td>
+                row.innerHTML =
+                    `
+                <th scope="row">${appointment.id}<th>
+                <td>${appointment.created_at}<td>
+                <td>${appointment.updated_at}<td>
                 <td>${appointment.title}<td>
                 <td>${appointment.name}<td>
                 <td>${appointment.lastname}<td>
@@ -25,27 +27,27 @@ document.addEventListener("DOMContentLoaded", function () {
                 <td>${appointment.mobilePhone}<td>
                 <td>${appointment.message}<td>
                 <td>
-                        <div class="btn-group">
-                            <button class="btn btn-sm btn-primary">
-                                <i class="fa-solid fa-pen-to-square"></i> Change
-                            </button>
-                            <button class="btn btn-sm btn-danger">
-                                <i class="fa-solid fa-trash-can"></i> Delete
-                            </button>
-                        </div>
-                    </td>
+                    <div class="btn-group">
+                        <button class="btn btn-sm btn-primary">
+                            <i class="fa-solid fa-pen-to-square"></i> Change
+                        </button>
+                        <button class="btn btn-sm btn-danger">
+                            <i class="fa-solid fa-trash-can"></i> Delete
+                        </button>
+                    </div>
+                </td>
                 `;
                 tableBody.appendChild(row);
             });
         })
         .catch(error => {
-            console.error("Error:" + error);
-            document.getElementById("appointmentTableBody").innerHTML = `
+            console.error("Error:", error.message);
+            document.getElementById('appointmentTableBody').innerHTML = `
                 <tr>
                     <td colspan="6" class="text-danger text-cented">
                         Error loading data: ${error.message}
                     </td>
                 </tr>
-            `
-        })
-})
+            `;
+        });
+});
