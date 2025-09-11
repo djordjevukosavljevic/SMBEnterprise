5b33cb84bf6b9408b2c0c0086be716103ae20bbd
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-09-11 13:25:07 +0200

    added sample controller appointment

--- src/main/java/rs/ac/university/gradjevinaAplikacija/Controller/AppointmentController.java
+++ src/main/java/rs/ac/university/gradjevinaAplikacija/Controller/AppointmentController.java
@@ -2,9 +2,15 @@ package rs.ac.university.gradjevinaAplikacija.Controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rs.ac.university.gradjevinaAplikacija.Entity.Appointment;
 import rs.ac.university.gradjevinaAplikacija.Service.AppointmentService;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/appointment")
@@ -16,5 +22,17 @@ public class AppointmentController
     public AppointmentController(AppointmentService appointmentService)
     {
         this.appointmentService = appointmentService;
+    }
+
+    @GetMapping
+    public List<Appointment> getAllAppointments()
+    {
+        return appointmentService.getAllAppointments();
+    }
+
+    @GetMapping(path = "/{id")
+    public Optional<Appointment> getAppointmentById(@PathVariable Integer id)
+    {
+        return appointmentService.getAppointmentById(id);
     }
 }
