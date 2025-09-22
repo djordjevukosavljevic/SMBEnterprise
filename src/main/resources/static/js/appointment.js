6412ebf21a7e51cd2993afbe42d630a3d6ba0fbd
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-09-22 15:20:02 +0200

    changes on appointment page

--- src/main/resources/static/js/appointment.js
+++ src/main/resources/static/js/appointment.js
@@ -1,7 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var calendarEl = document.getElementById("calendar");
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth'
+        initialView: 'dayGridWeek',
+        // Trying to customize from here
+        themeSystem: 'bootstrap5',
+        editable: true, 
+        selectable: true,
+        customButtons: {
+            bookNow: {
+                text: "Book an appointment",
+                click: function(){
+
+                }
+            }
+        }
+        // -> to here
     });
     calendar.render();
 })
