e17feb6b248838715df80e70ddc93754e6ec2d97
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-10-07 18:21:01 +0200

    added neccessary changes on appointment

--- src/main/resources/static/js/appointment.js
+++ src/main/resources/static/js/appointment.js
@@ -21,38 +21,45 @@ document.addEventListener("DOMContentLoaded", function () {
     calendar.render();
 })
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("bookform").addEventListener("submit",   async function e() {
-        e.preventDefault();
+
+document.getElementById("bookbtn").onclick = function(){
+    window.alert("We are working on this feature...");
+    wundow.location.reload();
+}
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.getElementById("bookform").addEventListener("submit",   async function e() {
+//         e.preventDefault();
         
-        const form = e.target;
-        const data = 
-        {   
-            name: form.name.value,
-            lastname: form.lastname.value,
-            email: form.email.value,
-            mobileNumber: form.mobileNumber.value,
-            subject: form.subject.value,
-            message: form.message.value,
-            dateCreated: form.dateCreated.value,
-            isactive: form.isactive.value,
-            istaken: form.istaken.value
-        };
-        try {
-            const response = await fetch("/api/appointment", {
-                method: "POST",
-                headers: { "Content-Type": "application/json"},
-                body: JSON.stringify(data)
-            });
+//         const form = e.target;
+//         const data = 
+//         {   
+//             name: form.name.value,
+//             lastname: form.lastname.value,
+//             email: form.email.value,
+//             mobileNumber: form.mobileNumber.value,
+//             subject: form.subject.value,
+//             message: form.message.value,
+//             dateCreated: form.dateCreated.value,
+//             isactive: form.isactive.value,
+//             istaken: form.istaken.value
+//         };
+//         try {
+//             const response = await fetch("/api/appointment", {
+//                 method: "POST",
+//                 headers: { "Content-Type": "application/json"},
+//                 body: JSON.stringify(data)
+//             });
             
-            const text = await response.text();
-            document.getElementById("status").innerText = text;
-            window.alert("Thank you for arranging an appointment, we will contact you soon");
-            form.reset();
-            window.location.reload();
-        }
-        catch(err){
-            document.getElementById("status").innerText = "Failed to book an appointment." + err.message;
-        }
-    });
-});
+//             const text = await response.text();
+//             document.getElementById("status").innerText = text;
+//             window.alert("Thank you for arranging an appointment, we will contact you soon");
+//             form.reset();
+//             window.location.reload();
+//         }
+//         catch(err){
+//             document.getElementById("status").innerText = "Failed to book an appointment." + err.message;
+//         }
+//     });
+// });
