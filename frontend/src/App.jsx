8ad2820350e552616c50194dc344c4efb12ab5c0
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-10-26 13:10:05 +0100

    added changes to App.jsx - Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer"
 
-
-function App(){
+function App() {
     return (
-        <Header />
-        // <Router>
-        //     <Header />
-        //     <Routes>
-        //         <Route path="" element={<Home />}/>
-
-        //     </Routes>
-        //     <Footer />
-        // </Router>
+        <Router>
+            <Header />
+            <Footer />
+        </Router>
     );
 }
