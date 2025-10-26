f570887f1e88367a2d53db0233d96bee11fc2b7d
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-10-26 13:06:08 +0100

    adding changes to frontend header and app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,14 @@ import Header from "./components/Header";
 
 function App(){
     return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="" element={<Home />}/>
-                {/* adding other pages here */}
-            </Routes>
-            <Footer />
-        </Router>
+        <Header />
+        // <Router>
+        //     <Header />
+        //     <Routes>
+        //         <Route path="" element={<Home />}/>
+
+        //     </Routes>
+        //     <Footer />
+        // </Router>
     );
 }
