038f375737240a27bd912f10a7145c80240a7074
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-10-26 22:03:57 +0100

    changes to frontend...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer"
+import Home from "./pages/Home"
 
 function App() {
     return (
-        <Router>
-            <Header />
-            
-            <Footer />
-        </Router>
+        <Header />
+
     );
 }
 
