7a2193bc857ea3632beb120440bb4cac3f0d1a90
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2025-11-07 01:37:27 +0100

    added changes

--- frontend/src/js/appointment.js
+++ frontend/src/js/appointment.js
@@ -3,6 +3,6 @@ export function bookFunction() {
     document.getElementById("bookbtn").onclick = function() 
     {
         window.alert("We are still working on this feature...");
-       window.location.href = "http://localhost:8083/#home"; 
+        window.location.href = "http://localhost:8083/#home"; 
     }
 }
