9bf68bb884b03748dff6fd3fde3738dddd6eae1d
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2026-01-15 22:27:37 +0100

    adding appointment

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import listPlugin from "@fullcalendar/list";
@@ -9,6 +9,53 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { bookFunction } from "../js/appointment.js";
 
 export default function Appoinmtent() {
+    const initialState = {
+        name: "",
+        lastname: "",
+        email: "",
+        mobileNumber: "",
+        subject: "",
+        message: "",
+        datecreated: "",
+        isActive: "",
+        isTaken: ""
+    }
+
+    const [formData, setFormData] = useState({
+        name: "",
+        lastname: "",
+        email: "",
+        mobileNumber: "",
+        subject: "",
+        message: "",
+        datecreated: "",
+        isActive: "",
+        isTaken: ""
+    });
+
+    const handleChange = async (e) => {
+        e.preventDefault();
+        try{
+            const response = await fetch("http://localhost:8082/api/appointment", {
+                method: "POST",
+                headers:{ 
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(formData),
+                });
+
+                if(!response.ok){
+                    throw new Error("Failed to send message");
+                }
+
+                setFormData(initialState);
+                window.location.href = "http://localhost:8083/#home"
+
+        } catch(error){
+            alert("Error sending message.");
+            console.error(error);
+        }
+    };
     return (
         <section id="appointment" className="containet py-5">
             <div className="container mt-4 mb-5">
@@ -25,12 +72,12 @@ export default function Appoinmtent() {
                         height="400px"
                     />
                     <div className="text-center">
-                        <br/><button onClick={bookFunction} id="bookbtn" type="submit" className="btn btn-outline-dark btn-lg mx-2">
+                        <br /><button onClick={bookFunction} id="bookbtn" type="submit" className="btn btn-outline-dark btn-lg mx-2">
                             Book <i className="fa-solid fa-envelope"></i>
                         </button>
 
                     </div>
-                    
+
                 </div>
 
 
@@ -46,6 +93,6 @@ export default function Appoinmtent() {
                 </form> */}
             </div>
         </section>
-        
+
     );
 }
