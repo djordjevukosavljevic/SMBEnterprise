26386b0694325e3393cdd6e0331ff7d43569355d
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2026-06-02 16:00:02 +0200

    adding appointment function

--- frontend/src/js/appointment.js
+++ frontend/src/js/appointment.js
@@ -1,5 +1,6 @@
 
-export function bookFunction() {
+export function bookFunction() 
+{
     document.getElementById("bookbtn").onclick = function() 
     {
         window.alert("We are still working on this feature...");
