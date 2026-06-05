55507aeac83e41896a0d72f125189f5a08a8026c
djordjevukosavljevic <djordje.vukosavljevic01@gmail.com>
2026-06-05 17:04:08 +0200

    trying to add something cool

--- src/main/java/rs/ac/university/gradjevinaAplikacija/Controller/ViewController.java
+++ src/main/java/rs/ac/university/gradjevinaAplikacija/Controller/ViewController.java
@@ -1,10 +1,17 @@
 package rs.ac.university.gradjevinaAplikacija.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/pages")
 public class ViewController
 {
+    @GetMapping
+    public String vision()
+    {
+        return "vision";
+    }
 
-    
 }
